5f5cd28c9e060a2241099540b58ef32fd3010b20
Licherr <47250417+Licherr@users.noreply.github.com>
2019-02-04 07:52:29 +0800

    Add files via upload

--- bot.js
+++ bot.js
@@ -1,7 +1,39 @@
-var botConfigs = {
-    token: "NDgyMTE4MTM2NDczMzIxNDcy.Dzj33Q.D5ZxGGnMMYoFWJQJ7KNOvrBSrr0",
-    prefix: "~",
-    gameStatus: "Rulling Hell",
-    commands: [{"id":1,"command":"aleowtf","message":"BUT NIGGA!","embed":false},{"id":2,"command":"aleo wtf","message":"but NIGGA!","embed":false},{"id":3,"command":"patdicksize","message":"unknown libtard","embed":false},{"id":6,"command":"whythefuck","message":"Yes","embed":false},{"id":6,"command":"summonAleo","message":"@Aleolakas#8306","embed":false}],
-    plugins: [{" "}],
-};
+var Discord = require('discord.io');
+var logger = require('winston');
+var auth = require('./auth.json');
+// Configure logger settings
+logger.remove(logger.transports.Console);
+logger.add(logger.transports.Console, {
+    colorize: true
+});
+logger.level = 'debug';
+// Initialize Discord Bot
+var bot = new Discord.Client({
+   token: auth.token,
+   autorun: true
+});
+bot.on('ready', function (evt) {
+    logger.info('Connected');
+    logger.info('Logged in as: ');
+    logger.info(bot.username + ' - (' + bot.id + ')');
+});
+bot.on('message', function (user, userID, channelID, message, evt) {
+    // Our bot needs to know if it will execute a command
+    // It will listen for messages that will start with `!`
+    if (message.substring(0, 1) == '!') {
+        var args = message.substring(1).split(' ');
+        var cmd = args[0];
+       
+        args = args.splice(1);
+        switch(cmd) {
+            // !ping
+            case 'ping':
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'Pong!'
+                });
+            break;
+            // Just add any case commands if you want to..
+         }
+     }
+});
--- start.bat
+++ start.bat
@@ -1,1 +1,2 @@
-npm start
+npm install discord.io winston --save
+node bot.js
